3dfa5a52c4a87bc9e80d85a653ae4d3200c5abd3
Shane <shanebrewer218@gmail.com>
2020-05-11 15:46:01 -0400

    EOL

--- datbot.js
+++ datbot.js
@@ -41,7 +41,7 @@ client.on("ready", () => {
     afk: false,
     status: "online",
     game: {
-      name: client.guilds.size + " servers | db.help",
+      name: "db.info",
       type: 3
     }
   })
@@ -52,7 +52,7 @@ client.on("guildCreate", guild => {
     afk: false,
     status: "online",
     game: {
-      name: client.guilds.size + " servers | db.help",
+      name: "db.info",
       type: 3
     }
   })
@@ -63,7 +63,7 @@ client.on("guildDelete", guild => {
     afk: false,
     status: "online",
     game: {
-      name: client.guilds.size + " servers | db.help",
+      name: "db.info",
       type: 3
     }
   })
@@ -100,149 +100,20 @@ client.on('message', async msg => {
    },
    "fields": [
      {
-       "name": "Example Command",
-       "value": "db.ping"
+       "name": "Important Information",
+       "value": "datbot is being retired. i dont find it to be a good bot anymore, and i dont want to rewrite the bot, as im working on a different one anyway."
      },
      {
-       "name": "Framework",
-       "value": "Discord.js 11.5.1 | Node.js 13.5.0"
+       "name": "New Bot",
+       "value": "a bot that could replace datbot: <https://discordapp.com/oauth2/authorize?client_id=499391082203840538&scope=bot&permissions=8>"
      }
     ]
   }})
   }
 
   if (command === 'help') {
-	msg.channel.send("Check your dms!");
-    msg.author.send({embed: {
-        color: embedcolor,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL,
-        },
-        title: "Help",
-        fields: [
-          {
-            name: "db.info",
-            value: "Gets info about datbot"
-          },
-          {
-            name: "db.ping",
-            value: "Useful for seeing if datbot is still rolling"
-          },
-          {
-            name: "db.rate",
-            value: "Use datbot to rate"
-          },
-          {
-            name: "db.say",
-            value: "Use datbot to spek"
-          },
-          {
-            name: "db.8ball",
-            value: "Use datbot 8ball magic stuff"
-          },
-          {
-            name: "db.reddit",
-            value: "datbot will fetch a random image from the subreddit provided"
-          },
-          {
-            name: "db.meme",
-            value: "datbot will provide only the finest of dank memes"
-          },
-          {
-            name: "db.boobs",
-            value: "Gets a picture of boobs, only works in nsfw text channels"
-          },
-          {
-            name: "db.ass",
-            value: "Gets a picture of ass, only works in nsfw text channels"
-          }
-        ],
-        timestamp: new Date(),
-        footer: {
-          icon_url: client.user.avatarURL,
-          text: "datbot ver " + botver
-        }
-      }
-    });
-    }
-
-  if (command === 'eval') {
-    if(message.author.id !== config.ownerid) return;
-
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-      msg.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      msg.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
+	msg.channel.send("Type db.info");
   }
-
-  if (command === 'rate') {
-  	var suffixrate = args.join(" ");
-  	if (suffixrate) {
-  	var num = Math.floor(Math.random() * 101);
-  	msg.reply('I rate ' + suffixrate + ': ' + num + '/100');
-    }
-  }
-
-  if (command === "say") {
-    const sayMessage = args.join(" ")
-    msg.delete().catch(O_o=>{})
-    msg.channel.send(sayMessage)
-  }
-
-  if (command === "8ball") {
-  	var suffix8ball = args.join(" ")
-  	var results = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Sings point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-  	var rng = Math.floor(Math.random() * 20)
-  	msg.reply("asks, " + "*" + suffix8ball + "*\n" + "```" + results[rng] + "```")
-  }
-
-  if (command === "meme") {
-    randomPuppy("dankmemes")
-      .then(url => {
-    		msg.reply(url);
-    })
-  }
-
-  if (command === 'reddit') {
-    var suffixreddit = args.join(" ");
-   	if(suffixreddit) {
-   		randomPuppy(suffixreddit)
-		    .then(url => {
-        	msg.reply(url);
-    })
-	 }
- }
-
-//nsfw commands
-  if (command === "boobs") {
-    if (msg.channel.nsfw === false) {
-    return msg.reply("To use this command, you must be in a NSFW text channel.");
-  } else {
-    randomPuppy("BustyPetite")
-  	.then(url => {
-      msg.reply(url);
-    })
-  }
-}
-
-if (command === "ass") {
-  if (msg.channel.nsfw === false) {
-  return msg.reply("To use this command, you must be in a NSFW text channel.");
-} else {
-  randomPuppy("ass")
-  .then(url => {
-    msg.reply(url);
-  })
-}
-}
 
 });
 
